0a29abe1ce78b48130a1f512d370a156261bd30e
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-03-25 19:51:50 +0000

    testing creation of table within database...

--- db.php
+++ db.php
@@ -3,26 +3,39 @@ $user = "root";
 $pass = "root";
 $dbname = "sys";
 $host = "localhost";
+
+function tableExists($connection, $dbname, $table) {
+    $statement = $connection->prepare("
+        SELECT COUNT(*) 
+        FROM INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_SCHEMA = ? 
+        AND TABLE_NAME = ?
+    ");
+    $statement->execute([$dbname, $table]);
+    return (bool)$statement->fetchColumn();
+}
+
+function createTable($connection, $dbname, $tablename) {
+    $statement = $connection->prepare("
+        CREATE TABLE $tablename (
+            column1 INT 
+        )
+        ");
+    $statement->execute();
+}
+
 try {
     $databaseConnection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     echo "Connection successfull!\n";
+    $table = "test";
+    if (tableExists($databaseConnection, $dbname, $table)) {
+        echo "Table Exists!";
+    } else {
+        createTable($databaseConnection, $dbname, $table);
+        echo "Table does not exist!";
+    }
 } catch (PDOException $e) {
     echo "Error, cannot connect to database!";
     // attempt to retry the connection after some timeout for example
-}
-
-if ($databaseConnection) {
-    $tableName = "test";
-    $sql = "SELECT TABLE_NAME
-            FROM INFORMATION_SCHEMA.TABLES 
-            WHERE TABLE_SCHEMA = 'sys'
-            AND TABLE_NAME = '$tableName'";
-    $result = $databaseConnection->query($sql);
-    // if ($result->num_rows > 0) {
-    //     echo "Table ($tableName) exists in the local database called $dbname!";
-    // } else {
-    //     echo "Does not exist!";
-    // }
-    // echo "Dean";
 }
 ?>
